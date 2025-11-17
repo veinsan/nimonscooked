f68394f163c7524ba5b41a6614acfcceec6b98f8
veinsan <riantamaputra751@gmail.com>
2025-11-18 01:49:32 +0700

    refactor: perubahan tipe atribut pada KitchenUtensil.java

--- src/main/java/com/nimonscooked/model/utensil/KitchenUtensil.java
+++ src/main/java/com/nimonscooked/model/utensil/KitchenUtensil.java
@@ -1,24 +1,31 @@
 package com.nimonscooked.model.utensil;
 
 import com.nimonscooked.model.Item;
-import com.nimonscooked.model.dish.Dish;
+import com.nimonscooked.model.ingredient.Preparable;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class KitchenUtensil extends Item {
-    protected Dish containedDish;
+    protected List<Preparable> contents;
 
     public KitchenUtensil(String name) {
         super(name);
+        this.contents = new ArrayList<>();
     }
 
-    public Dish getContainedDish() {
-        return containedDish;
+    public List<Preparable> getContents() {
+        return new ArrayList<>(contents);
     }
 
-    public void setContainedDish(Dish dish) {
-        this.containedDish = dish;
+    public void addContent(Preparable ingredient) {
+        contents.add(ingredient);
+    }
+    
+    public void clearContents() {
+        contents.clear();
     }
     
     public boolean isEmpty() {
-        return containedDish == null;
+        return contents.isEmpty();
     }
 }
