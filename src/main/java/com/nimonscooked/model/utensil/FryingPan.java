e96d515e9cb22ae548ca953f92176f459503416d
veinsan <riantamaputra751@gmail.com>
2025-11-18 01:51:10 +0700

    feat: implementasi kode FryingPan.java

--- src/main/java/com/nimonscooked/model/utensil/FryingPan.java
+++ src/main/java/com/nimonscooked/model/utensil/FryingPan.java
@@ -1,4 +1,51 @@
 package com.nimonscooked.model.utensil;
 
-public class FryingPan extends KitchenUtensil {
+import com.nimonscooked.model.ingredient.Preparable;
+import com.nimonscooked.model.ingredient.Ingredient;
+
+public class FryingPan extends KitchenUtensil implements CookingDevice {
+    
+    private static final int MAX_CAPACITY = 1;
+    
+    public FryingPan() {
+        super("Frying Pan");
+    }
+
+    @Override
+    public boolean isPortable() {
+        return true;
+    }
+
+    @Override
+    public int capacity() {
+        return MAX_CAPACITY;
+    }
+
+    @Override
+    public boolean canAccept(Preparable ingredient) {
+        if (ingredient instanceof Ingredient) {
+            Ingredient ing = (Ingredient) ingredient;
+            return ing.getState() == Ingredient.State.CHOPPED 
+                   && contents.size() < MAX_CAPACITY;
+        }
+        return false;
+    }
+
+    @Override
+    public void addIngredient(Preparable ingredient) {
+        if (canAccept(ingredient)) {
+            contents.add(ingredient);
+        } else {
+            throw new IllegalArgumentException("Cannot add this ingredient to frying pan");
+        }
+    }
+
+    @Override
+    public void startCooking() {
+        for (Preparable p : contents) {
+            if (p.canBeCooked()) {
+                p.cook();
+            }
+        }
+    }
 }
