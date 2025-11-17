3451316071c41389c145b984068c4a46a069532a
veinsan <riantamaputra751@gmail.com>
2025-11-18 01:46:33 +0700

    feat: update Ingredients.java (add implements preparable)

--- src/main/java/com/nimonscooked/model/ingredient/Ingredient.java
+++ src/main/java/com/nimonscooked/model/ingredient/Ingredient.java
@@ -2,8 +2,8 @@ package com.nimonscooked.model.ingredient;
 
 import com.nimonscooked.model.Item;
 
-public class Ingredient extends Item {
-
+public class Ingredient extends Item implements Preparable {
+    
     public enum State {
         RAW, CHOPPED, COOKED
     }
@@ -21,5 +21,38 @@ public class Ingredient extends Item {
 
     public void setState(State s) {
         this.state = s;
+    }
+    
+    @Override
+    public boolean canBeChopped() {
+        return state == State.RAW;
+    }
+
+    @Override
+    public boolean canBeCooked() {
+        return state == State.RAW || state == State.CHOPPED;
+    }
+
+    @Override
+    public boolean canBePlacedOnPlate() {
+        return state == State.CHOPPED || state == State.COOKED;
+    }
+
+    @Override
+    public void chop() {
+        if (canBeChopped()) {
+            this.state = State.CHOPPED;
+        } else {
+            throw new IllegalStateException("Cannot chop ingredient in state: " + state);
+        }
+    }
+
+    @Override
+    public void cook() {
+        if (canBeCooked()) {
+            this.state = State.COOKED;
+        } else {
+            throw new IllegalStateException("Cannot cook ingredient in state: " + state);
+        }
     }
 }
