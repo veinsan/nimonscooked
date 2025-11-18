3ee382b9ef8fa77dd519e37a74f87a84227f6edc
pn-local <18224079@std.stei.itb.ac.id>
2025-11-18 18:16:16 +0700

    feat: implements Preparable to Dish

--- src/main/com/nimonscooked/model/dish/Dish.java
+++ src/main/com/nimonscooked/model/dish/Dish.java
@@ -1,10 +1,12 @@
 package com.nimonscooked.model.dish;
 
+import com.nimonscooked.model.Item;
+import com.nimonscooked.model.ingredient.Preparable;
 import java.util.ArrayList;
 import java.util.List;
-import com.nimonscooked.model.Item;
 
-public class Dish extends Item {
+public class Dish extends Item implements Preparable{
+
     private List<Item> components;
 
     public Dish(String name, List<Item> components) {
@@ -15,7 +17,7 @@ public class Dish extends Item {
     public List<Item> getComponents() {
         return new ArrayList<>(components);
     }
-    
+
     public void addComponent(Item item) {
         this.components.add(item);
     }
