8c730e3128861db28785806b6743ff39bd679c06
veinsan <riantamaputra751@gmail.com>
2025-11-20 12:05:22 +0700

    feat: implementasi BakingStrategy.java

--- src/main/java/com/nimonscooked/strategy/BakingStrategy.java
+++ src/main/java/com/nimonscooked/strategy/BakingStrategy.java
@@ -1,5 +1,32 @@
 package com.nimonscooked.strategy;
 
-public class BakingStrategy {
+import com.nimonscooked.interfaces.Preparable;
+import com.nimonscooked.model.ingredient.Ingredient;
+import com.nimonscooked.model.ingredient.IngredientState;
+
+public class BakingStrategy implements CookingStrategy {
     
+    @Override
+    public boolean canCook(Preparable ingredient) {
+        if (!(ingredient instanceof Ingredient)) {
+            return false;
+        }
+        Ingredient ing = (Ingredient) ingredient;
+        String name = ing.getName().toLowerCase();
+        return (name.contains("pizza") || name.contains("dough") || name.contains("adonan"))
+               && ing.getState() == IngredientState.CHOPPED;
+    }
+
+    @Override
+    public void cook(Preparable ingredient) {
+        if (canCook(ingredient)) {
+            ingredient.cook();
+            System.out.println("🍕 Baking: " + ((Ingredient) ingredient).getName());
+        }
+    }
+
+    @Override
+    public String getMethodName() {
+        return "Baking";
+    }
 }
