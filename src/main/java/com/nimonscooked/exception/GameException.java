2a1a175d40f02098e6a04f60c41f9d9d719288e6
veinsan <riantamaputra751@gmail.com>
2025-11-20 12:09:05 +0700

    feat: implementasi exception

--- src/main/java/com/nimonscooked/exception/GameException.java
+++ src/main/java/com/nimonscooked/exception/GameException.java
@@ -1,5 +1,21 @@
 package com.nimonscooked.exception;
 
-public class GameException {
+public class GameException extends RuntimeException {
     
+    public GameException(String message) {
+        super(message);
+    }
+
+    public GameException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GameException(Throwable cause) {
+        super(cause);
+    }
+
+    @Override
+    public String toString() {
+        return "GameException: " + getMessage();
+    }
 }
--- src/main/java/com/nimonscooked/exception/StationFullException.java
+++ src/main/java/com/nimonscooked/exception/StationFullException.java
@@ -1,5 +1,32 @@
 package com.nimonscooked.exception;
 
-public class StationFullException {
+public class StationFullException extends GameException {
     
+    private final String stationId;
+    private final String stationType;
+
+    public StationFullException(String stationId, String stationType) {
+        super("Station '" + stationId + "' (" + stationType + ") is full!");
+        this.stationId = stationId;
+        this.stationType = stationType;
+    }
+
+    public StationFullException(String message) {
+        super(message);
+        this.stationId = "Unknown";
+        this.stationType = "Unknown";
+    }
+
+    public String getStationId() {
+        return stationId;
+    }
+
+    public String getStationType() {
+        return stationType;
+    }
+
+    @Override
+    public String toString() {
+        return "StationFullException: Station='" + stationId + "' Type='" + stationType + "'";
+    }
 }
