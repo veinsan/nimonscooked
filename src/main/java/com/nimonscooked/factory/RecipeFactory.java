a9599c7bcdafe04a3d220bc1ad200411bda11694
veinsan <riantamaputra751@gmail.com>
2025-11-20 12:08:16 +0700

    feat: implementasi file pada factory

--- src/main/java/com/nimonscooked/factory/RecipeFactory.java
+++ src/main/java/com/nimonscooked/factory/RecipeFactory.java
@@ -1,5 +1,150 @@
 package com.nimonscooked.factory;
 
+import com.nimonscooked.model.recipe.Recipe;
+import com.nimonscooked.model.recipe.RecipeBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecipeFactory {
     
+    private static RecipeFactory instance;
+
+    private RecipeFactory() {}
+
+    public static RecipeFactory getInstance() {
+        if (instance == null) {
+            synchronized (RecipeFactory.class) {
+                if (instance == null) {
+                    instance = new RecipeFactory();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public List<Recipe> createBurgerMenu() {
+        List<Recipe> menu = new ArrayList<>();
+        menu.add(createClassicBurger());
+        menu.add(createCheeseburger());
+        menu.add(createBLTBurger());
+        menu.add(createDeluxeBurger());
+        return menu;
+    }
+
+    public Recipe createClassicBurger() {
+        return RecipeBuilder.create("Classic Burger")
+            .addRawIngredient("Bread")
+            .addCookedIngredient("Meat")
+            .build();
+    }
+
+    public Recipe createCheeseburger() {
+        return RecipeBuilder.create("Cheeseburger")
+            .addRawIngredient("Bread")
+            .addCookedIngredient("Meat")
+            .addChoppedIngredient("Cheese")
+            .build();
+    }
+
+    public Recipe createBLTBurger() {
+        return RecipeBuilder.create("BLT Burger")
+            .addRawIngredient("Bread")
+            .addChoppedIngredient("Lettuce")
+            .addChoppedIngredient("Tomato")
+            .addCookedIngredient("Meat")
+            .build();
+    }
+
+    public Recipe createDeluxeBurger() {
+        return RecipeBuilder.create("Deluxe Burger")
+            .addRawIngredient("Bread")
+            .addChoppedIngredient("Lettuce")
+            .addCookedIngredient("Meat")
+            .addChoppedIngredient("Cheese")
+            .build();
+    }
+
+    public List<Recipe> createSushiMenu() {
+        List<Recipe> menu = new ArrayList<>();
+        
+        menu.add(RecipeBuilder.create("Kappa Maki")
+            .addRawIngredient("Nori")
+            .addCookedIngredient("Rice")
+            .addChoppedIngredient("Cucumber")
+            .build());
+
+        menu.add(RecipeBuilder.create("Sakana Maki")
+            .addRawIngredient("Nori")
+            .addCookedIngredient("Rice")
+            .addRawIngredient("Fish")
+            .build());
+
+        menu.add(RecipeBuilder.create("Ebi Maki")
+            .addRawIngredient("Nori")
+            .addCookedIngredient("Rice")
+            .addCookedIngredient("Shrimp")
+            .build());
+
+        menu.add(RecipeBuilder.create("Fish Cucumber Roll")
+            .addRawIngredient("Nori")
+            .addCookedIngredient("Rice")
+            .addRawIngredient("Fish")
+            .addChoppedIngredient("Cucumber")
+            .build());
+
+        return menu;
+    }
+
+    public List<Recipe> createPastaMenu() {
+        List<Recipe> menu = new ArrayList<>();
+        
+        menu.add(RecipeBuilder.create("Pasta Marinara")
+            .addCookedIngredient("Pasta")
+            .addCookedIngredient("Tomato")
+            .build());
+
+        menu.add(RecipeBuilder.create("Pasta Bolognese")
+            .addCookedIngredient("Pasta")
+            .addCookedIngredient("Meat")
+            .build());
+
+        menu.add(RecipeBuilder.create("Pasta Frutti di Mare")
+            .addCookedIngredient("Pasta")
+            .addCookedIngredient("Shrimp")
+            .addCookedIngredient("Fish")
+            .build());
+
+        return menu;
+    }
+
+    public List<Recipe> createPizzaMenu() {
+        List<Recipe> menu = new ArrayList<>();
+        
+        menu.add(RecipeBuilder.create("Pizza Margherita")
+            .addChoppedIngredient("Dough")
+            .addChoppedIngredient("Tomato")
+            .addChoppedIngredient("Cheese")
+            .build());
+
+        menu.add(RecipeBuilder.create("Pizza Sosis")
+            .addChoppedIngredient("Dough")
+            .addChoppedIngredient("Tomato")
+            .addChoppedIngredient("Cheese")
+            .addChoppedIngredient("Sausage")
+            .build());
+
+        menu.add(RecipeBuilder.create("Pizza Ayam")
+            .addChoppedIngredient("Dough")
+            .addChoppedIngredient("Tomato")
+            .addChoppedIngredient("Cheese")
+            .addChoppedIngredient("Chicken")
+            .build());
+
+        return menu;
+    }
+
+    public Recipe createCustomRecipe(String name, RecipeBuilder builder) {
+        return builder.setName(name).build();
+    }
 }
--- src/main/java/com/nimonscooked/factory/StationFactory.java
+++ src/main/java/com/nimonscooked/factory/StationFactory.java
@@ -1,5 +1,110 @@
 package com.nimonscooked.factory;
 
+import com.nimonscooked.model.station.*;
+import com.nimonscooked.model.recipe.Recipe;
+import com.nimonscooked.exception.InvalidActionException;
+
+import java.util.List;
+
 public class StationFactory {
     
+    private static StationFactory instance;
+
+    private StationFactory() {}
+
+    public static StationFactory getInstance() {
+        if (instance == null) {
+            synchronized (StationFactory.class) {
+                if (instance == null) {
+                    instance = new StationFactory();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Station createStation(String type, String id) {
+        return createStation(type, id, null, null);
+    }
+
+    public Station createStation(String type, String id, String ingredientType, List<Recipe> recipes) {
+        if (type == null || type.isEmpty()) {
+            throw new InvalidActionException("Station type cannot be null or empty");
+        }
+
+        switch (type.toUpperCase()) {
+            case "C":
+            case "CUTTING":
+                return new CuttingStation(id);
+
+            case "R":
+            case "COOKING":
+                return new CookingStation(id);
+
+            case "A":
+            case "ASSEMBLY":
+                if (recipes == null) {
+                    throw new InvalidActionException("Assembly station requires recipes");
+                }
+                return new AssemblyStation(id, recipes);
+
+            case "I":
+            case "INGREDIENT":
+                if (ingredientType == null || ingredientType.isEmpty()) {
+                    throw new InvalidActionException("Ingredient storage requires ingredient type");
+                }
+                return new IngredientStorage(id, ingredientType);
+
+            case "P":
+            case "PLATE":
+                return new PlateStorage(id);
+
+            case "S":
+            case "SERVING":
+                return new ServingCounter(id);
+
+            case "W":
+            case "WASHING":
+                return new WashingStation(id);
+
+            case "T":
+            case "TRASH":
+                return new TrashStation(id);
+
+            default:
+                throw new InvalidActionException("Unknown station type: " + type);
+        }
+    }
+
+    public CuttingStation createCuttingStation(String id) {
+        return new CuttingStation(id);
+    }
+
+    public CookingStation createCookingStation(String id) {
+        return new CookingStation(id);
+    }
+
+    public AssemblyStation createAssemblyStation(String id, List<Recipe> recipes) {
+        return new AssemblyStation(id, recipes);
+    }
+
+    public IngredientStorage createIngredientStorage(String id, String ingredientType) {
+        return new IngredientStorage(id, ingredientType);
+    }
+
+    public PlateStorage createPlateStorage(String id) {
+        return new PlateStorage(id);
+    }
+
+    public ServingCounter createServingCounter(String id) {
+        return new ServingCounter(id);
+    }
+
+    public WashingStation createWashingStation(String id) {
+        return new WashingStation(id);
+    }
+
+    public TrashStation createTrashStation(String id) {
+        return new TrashStation(id);
+    }
 }
