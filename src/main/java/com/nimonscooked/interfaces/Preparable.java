f68fc438165d501e58be01b5f4ea6b20a23b6f21
pn-local <18224079@std.stei.itb.ac.id>
2025-11-24 17:19:40 +0700

    fix: moved enum State to Preparable to make state checking easier in utensils

--- src/main/java/com/nimonscooked/interfaces/Preparable.java
+++ src/main/java/com/nimonscooked/interfaces/Preparable.java
@@ -1,9 +1,15 @@
 package com.nimonscooked.interfaces;
 
 public interface Preparable {
+    enum State {
+        RAW,
+        CHOPPED,
+        COOKED,
+    }
     boolean canBeChopped();
     boolean canBeCooked();
     boolean canBePlacedOnPlate();
     void chop();
     void cook();
+    State getState();
 }
