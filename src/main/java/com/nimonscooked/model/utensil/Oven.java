0941644ecd03c55d04f840f8c9488005d3cfd735
pn-local <18224079@std.stei.itb.ac.id>
2025-11-24 17:28:15 +0700

    feat: implement Preparable in Oven (placeholders since Oven is unused)

--- src/main/java/com/nimonscooked/model/utensil/Oven.java
+++ src/main/java/com/nimonscooked/model/utensil/Oven.java
@@ -1,5 +1,8 @@
 package com.nimonscooked.model.utensil;
 
+import com.nimonscooked.interfaces.CookingDevice;
+import com.nimonscooked.interfaces.Preparable;
+
 public class Oven extends KitchenUtensil implements CookingDevice {
 
     public Oven() {
@@ -8,5 +11,21 @@ public class Oven extends KitchenUtensil implements CookingDevice {
 
     public boolean isPortable() {
         return false;
+    }
+
+    public int capacity() { return 1; }
+
+    public boolean canAccept(Preparable ingredient) { return false; }
+
+    public void addIngredient(Preparable ingredient) {
+        if (canAccept(ingredient)) {
+            contents.add(ingredient);
+        }
+    }
+
+    public void startCooking() {
+        for (Preparable p : contents) {
+            p.cook();
+        }
     }
 }
